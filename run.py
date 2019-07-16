635c91764e49042991c0524f0bae0d144173cfb3
SlackCrow <jasonkim@linux.com>
2019-07-17 01:22:18 +0900

    Update run.py

--- run.py
+++ run.py
@@ -1,15 +1,39 @@
-from SimpleWebSocketServer import SimpleWebSocketServer, WebSocket
+from flask import Flask, render_template,request
+from flask_socketio import SocketIO, join_room, emit
+import random
+import json
+from flask_cors import CORS
 
-class aruServer(WebSocket):
-    clients = []
-    def handleConnected(self):
-        print(self.address, 'connected')
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret!'
+socketio = SocketIO(app)
+CORS(app)
 
-    def handleClose(self):
-        print(self.address, 'disconnected')
+connectedClients = []
+secure_random = random.SystemRandom()
 
-def main():
-    server = SimpleWebSocketServer('', 3000, aruServer)
-    server.serveforever()
+roomList = ['general','tech']
 
-if __name__ == "__main__": main()
+@app.route("/")
+def index():
+    if request.remote_addr in connectedClients:
+        return render_template('index.html')
+    else:
+        return render_template('login.html')
+        
+@app.route('/createAccount', methods=['GET', 'POST'])
+def createAccount():
+    if request.method == 'POST':
+        pass
+        return redirect("http://localhost:5000/login", code=302)
+    return render_template('create_account.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+            pass
+        else:
+            error = 'Invalid Credentials. Please try again.'
+        return redirect("http://localhost:5000/", code=302)
+    return render_template('login.html', error=error)
